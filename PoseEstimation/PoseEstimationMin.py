41687440fe083097e53e96eca7ab935b5a1642c0
Mathvivas <mathvivas@hotmail.com>
2024-01-02 17:59:35 -0300

    Showing pose connections

--- PoseEstimation/PoseEstimationMin.py
+++ PoseEstimation/PoseEstimationMin.py
@@ -6,7 +6,7 @@ mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
-cap = cv2.VideoCapture('videos/2.mp4')
+cap = cv2.VideoCapture('videos/4.mp4')
 pTime = 0
 
 while True:
@@ -16,6 +16,10 @@ while True:
     results = pose.process(imgRGB)
     if results.pose_landmarks:
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)

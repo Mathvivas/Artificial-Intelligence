351f111ad85222ff06374651a452abaaec6f5c21
Mathvivas <mathvivas@hotmail.com>
2024-01-02 15:00:15 -0300

    Finished basic code, detecting hand and can detect one specific part of the hand

--- HandTracking/HandTrackingMin.py
+++ HandTracking/HandTrackingMin.py
@@ -18,8 +18,21 @@ while True:
     # Tells if it detects a hand or not
     #print(results.multi_hand_landmarks)
 
+    # If there is a hand
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            # Shows the number for every part of the hand
+            for id, lm in enumerate(handLms.landmark):
+                #print(id, lm)
+                h, w, c = img.shape
+                # Multiplying x and y hand parts coordinates by their weights and heights
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                #print(id, cx, cy)
+                # Detecting one of the landmarks (only one finger, for example)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+
+            # Shows hands' lines
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()

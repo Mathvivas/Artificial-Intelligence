c5d169fb725bdddb27cd3f0504d88464517aac70
Mathvivas <mathvivas@hotmail.com>
2024-01-03 17:12:35 -0300

    Complete Project

--- PoseEstimation/PoseModule.py
+++ PoseEstimation/PoseModule.py
@@ -24,22 +24,38 @@ class PoseDetector():
     def findPose(self, img, draw=True):
         img = cv2.resize(img, (800, 600))
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.pose.process(imgRGB)
-        if results.pose_landmarks:
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
             if draw:
-                self.mpDraw.draw_landmarks(img, results.pose_landmarks, 
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, 
                                         self.mpPose.POSE_CONNECTIONS)
         return img
+    
+    def findPosition(self, img, draw=True):
+        lmlist = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmlist.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+        return lmlist
 
 
 def main():
-    cap = cv2.VideoCapture('videos/4.mp4')
+    cap = cv2.VideoCapture('videos/2.mp4')
     pTime = 0
     detector = PoseDetector()
 
     while True:
         success, img = cap.read()
         img = detector.findPose(img)
+        lmlist = detector.findPosition(img)
+        if len(lmlist) != 0:
+            cv2.circle(img, (lmlist[20][1], lmlist[20][2]), 10, (0, 0, 255), 
+                    cv2.FILLED)
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)

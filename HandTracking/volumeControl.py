309ffa4a1f87237f3b8502a355647478c0e2e193
Mathvivas <mathvivas@hotmail.com>
2024-01-05 11:37:41 -0300

    Volume Control Project, already showing the two fingers that will be used to change the volume

---
+++ HandTracking/volumeControl.py
@@ -0,0 +1,27 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+
+wCam, hCam = 1280, 720
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.HandDetector()
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), 
+                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 3)
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)

31a5989a4e4facf94c6cd0d9c0dc2358732ca13e
Mathvivas <mathvivas@hotmail.com>
2024-01-05 12:00:48 -0300

    Hand volume control library only works on Windows

--- HandTracking/volumeControl.py
+++ HandTracking/volumeControl.py
@@ -2,6 +2,10 @@ import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 wCam, hCam = 1280, 720
 
@@ -10,11 +14,47 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
-detector = htm.HandDetector()
+detector = htm.HandDetector(detectionConfidence=0.7)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None
+)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
+    lmlist = detector.findPosition(img, draw=False)
+    if len(lmlist) != 0:
+        #print(lmlist[4], lmlist[8])
+
+        x1, y1 = lmlist[4][1], lmlist[4][2]
+        x2, y2 = lmlist[8][1], lmlist[8][2]
+        # Center of the line
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        # Length of the line
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        #### Hand range 50 - 300
+        #### Volume range -65 - 0
+        # Converting hand range to volume range with numpy
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
